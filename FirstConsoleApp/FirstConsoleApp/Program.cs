1ba07577c7dab6f0774fc4fc5b36640d863a32d4
Óscar Oyaga <84621140+Oscar-oy@users.noreply.github.com>
2026-01-13 14:11:47 +0100

    Add files via upload

---
+++ FirstConsoleApp/FirstConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace FirstConsoleApp
+{
+    /// <summary>
+    /// First Console App lecture code
+    /// </summary>
+    internal class Program
+    {
+    /// <summary>
+    /// First Console App lecture code
+    /// </summary>
+    /// <param name="args">command-line args</param>
+        static void Main(string[] args)
+        {
+            //print a supportive message
+            Console.WriteLine("Hello, n00b!");
+        }
+    }
+}

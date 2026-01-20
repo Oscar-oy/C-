969049e8b3259fc7c42ff076285fddda2878de89
Óscar Oyaga <84621140+Oscar-oy@users.noreply.github.com>
2026-01-20 11:58:36 +0100

    Add files via upload
    This is an app where we are displaying into the console Data from a database. Those datas has been previouly builded in a view into SQL

---
+++ Master-C-Sharp-And-SQL-By-Building-Applications-master/App1 - Customer Order Viewer/CustomerOrderViewer/Models/CustomerOrderDetailModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomerOrderViewer.Models
+{
+    internal class CustomerOrderDetailModel
+    {
+        public int CustomerOrderId { get; set; }
+        public int CustomerId { get; set; }
+        public int ItemId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Description { get; set; }
+        public int MyProperty { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ Master-C-Sharp-And-SQL-By-Building-Applications-master/App1 - Customer Order Viewer/CustomerOrderViewer/Program.cs
@@ -0,0 +1,36 @@
+﻿using CustomerOrderViewer.Models;
+using CustomerOrderViewer.Repository;
+using System.Data.SqlClient;
+
+namespace CustomerOrderViewer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try { 
+                CustomerOrderDetailCommand customerOrderDetailCommand = new CustomerOrderDetailCommand(@"Data Source=localhost\SQLEXPRESS;Initial Catalog=CustomerOrderViewer;Integrated Security=True");
+
+                IList<CustomerOrderDetailModel> customerOrderDetailModels = customerOrderDetailCommand.GetList();
+
+                if (customerOrderDetailModels.Any())
+                {
+                    foreach(CustomerOrderDetailModel customerOrderDetailModel in customerOrderDetailModels)
+                    {
+                        Console.WriteLine("{0}: fullname: {1} {2} (Id: {3}) - purchased {4} for {5} (Id: {6})",
+                            customerOrderDetailModel.CustomerOrderId,
+                            customerOrderDetailModel.FirstName,
+                            customerOrderDetailModel.LastName,
+                            customerOrderDetailModel.CustomerId,
+                            customerOrderDetailModel.Description,
+                            customerOrderDetailModel.Price,
+                            customerOrderDetailModel.ItemId
+                            );
+                    }
+                };
+            } catch(Exception ex) {
+                Console.WriteLine("Something went wrong {0}", ex.Message);
+            }
+        }
+    }
+}

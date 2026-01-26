6941e02eb0e3e8845f765d705a58b3e6339771d7
Óscar Oyaga <84621140+Oscar-oy@users.noreply.github.com>
2026-01-26 17:04:28 +0100

    Refactor comments and improve code clarity
    Updated comments for clarity and improved code structure.

--- Master-C-Sharp-And-SQL-By-Building-Applications-master/App2 - Customer Order Viewer 2.0/CustomerOrderViewer2.0/CustomerOrderViewer2.0/Program.cs
+++ Master-C-Sharp-And-SQL-By-Building-Applications-master/App2 - Customer Order Viewer 2.0/CustomerOrderViewer2.0/CustomerOrderViewer2.0/Program.cs
@@ -9,6 +9,7 @@ namespace CustomerOrderViewer2._0
     internal class Program
     {
         /// <summary>
+        /// Learning how to manage SQL databases and creating commands to connect with them.
         /// Empezamos el programa declarando las variables privadas de la clase. 
         /// Definimos la conexión a la base de datos, y después definimos cada command. 
         /// </summary>
@@ -144,3 +145,4 @@ namespace CustomerOrderViewer2._0
         }
     }
 }
+
